edba90ee0c298954500aee714a138ef5cb15f90e
Kaustuv Karki <karkikaustuv@gmail.com>
2024-06-23 15:28:49 +0545

    Prisma setup and first migration

---
+++ prisma/migrations/20240623094249_user_table/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "Users" (
+    "id" TEXT NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "password" TEXT NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "profile" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Users_email_key" ON "Users"("email");
