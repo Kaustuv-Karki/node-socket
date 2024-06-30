1301aa8901b84fb4b2307941b2ac4f0392906cb1
Kaustuv Karki <karkikaustuv@gmail.com>
2024-06-30 21:40:02 +0545

    Socket configuration

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import express from "express";
 import "dotenv/config";
 import ApiRoutes from "./routes/api.js";
+import http from "http";
+import { fileURLToPath } from "url";
+import path from "path";
+
+import { Server } from "socket.io";
+
+const io = new Server(http);
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT || 8000;
 
@@ -10,6 +19,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api", ApiRoutes);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
